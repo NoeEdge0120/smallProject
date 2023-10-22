cc2d1e969afe52c95e3e54784668d83cbc0b8a33
離緣 <jeffyang0123@gmail.com>
2023-10-22 16:05:30 +0000

    2023-10-23 完成原始資料輸入和處理

---
+++ 2023-10/apcs-matrix.py
@@ -0,0 +1,35 @@
+inp = input('設定二維矩陣大小：')
+
+rule = inp.split()
+
+row:int = int(rule[0]) # 橫排
+column:int = int(rule[1]) # 直排
+
+data = input('輸入矩陣資料：')
+
+lData = data.split()
+temp:str = ''
+x:int
+y:int
+result:str = ''
+index:int = 0
+
+print(lData)
+
+dataNo:int = 0
+
+matrix: dict = {}
+
+for x in range(column):
+	print('\n')
+	temp:str = ""
+	x += 1
+	for y in range(row):
+		temp = f'{temp} {lData[index]}'
+		index += 1
+	# result = f'{result} \n {temp}'
+
+	temp.strip(' ')
+	matrix[x] = temp
+
+	print(f'x={x} y={y}\nlData: {lData}\ntemp: {temp}\nmatrix: {matrix}')
